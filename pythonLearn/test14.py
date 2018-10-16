1875c0c8efe2da712be139175c8b5a261f62dd95
yanghaipeng <haipeng.yang@yuntu-inc.com>
2018-10-16 20:10:57 +0800

    async IO

---
+++ pythonLearn/test14.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*-coding: UTF-8 -*-
+
+# 异步IO
+
+# 协程----------------------------------------
+
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print ('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+def produce(c):
+    c.send(None)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print ('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
+
+c = consumer()
+produce(c)
